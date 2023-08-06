d74305a71c115a3d61394389e6e81b81d888b396
Errik Rose <errose@student.fullsail.edu>
2023-08-05 19:25:29 -0500

    updated api/server.js with some basic error handling and cors

--- api/server.js
+++ api/server.js
@@ -5,8 +5,17 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+// CORS middleware
+app.use(cors());
+
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from the API!" });
+});
+
+// Basic error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong!" });
 });
 
 app.listen(port, () => {
